3f1ce3c4812e698b7e59b90b62152ca905b972b7
Nutchanon Chantrasup <nutchanonc>
2021-11-12 18:35:48 +0700

    set teams limit

--- src/commands/team.js
+++ src/commands/team.js
@@ -2,11 +2,31 @@ const { MessageEmbed } = require('discord.js');
 const getRandomInt = require("../libraries/random");
 
 function team(msg, args) {
-    
-    
     try {
         const teamNumber = parseInt(args[0]);
         const participants = args.slice(1,args.length).filter(player => player !== "");
+        if (!participants.length) {
+            const errorEmbed = new MessageEmbed()
+                .setColor('#f51848')
+                .setTitle('Please provide any participants')
+                .setDescription(`[🙁] We can't team up the empty participant for you!`)
+            return msg.channel.send({embeds: [errorEmbed]})
+        }
+        if (teamNumber > participants.length) {
+            const limitEmbed = new MessageEmbed()
+                .setColor('#f51848')
+                .setTitle('Are you serious?')
+                .setDescription(`[🙁] You are going to split in to ${teamNumber} team(s) which is more than the actual participants.`)
+            return msg.channel.send({embeds: [limitEmbed]})
+        }
+        if (teamNumber > 128) {
+            const limitEmbed = new MessageEmbed()
+                .setColor('#f51848')
+                .setTitle('Teams has been limited at 128!')
+                .setDescription(`[🙁] You can not spilt your participants in to more than 128 teams because of memory limitation on production deployment.`)
+            return msg.channel.send({embeds: [limitEmbed]})
+        }
+       
         const teams = []
         const marked = []
         let index = 0
@@ -36,6 +56,12 @@ function team(msg, args) {
         msg.channel.send({ embeds: [embed] });
     } catch (error) {
         console.log(error)
+        const errorEmbed = new MessageEmbed()
+                .setColor('#f51848')
+                .setTitle('There is an error occurs while teaming up.')
+                .setDescription("`"+`${error}`+"`")
+                .setFooter('[Tips] Team number must be integer and participants name can be a string without space(" ") or a Discord tag(@user), split each argument with space(" ").')
+        msg.channel.send({embeds: [errorEmbed]})
     }
     
 }
